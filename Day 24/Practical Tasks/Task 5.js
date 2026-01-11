994e2e4207dbf83b8966436cbf44b64ab510ad09
DEEPAN U <deepanudhayakumar01@gmail.com>
2026-01-11 20:06:54 +0530

    update

---
+++ Day 24/Practical Tasks/Task 5.js
@@ -0,0 +1,15 @@
+let age = 16;
+
+if (age > 18) {
+  console.log("You are eligible to vote");
+} else {
+  console.log("You are not eligible to vote");
+}
+
+// A logical error means:
+
+// The program runs without errors
+
+// But the output is wrong
+
+// This happens due to wrong conditions, formulas, or logic, not syntax mistakes.

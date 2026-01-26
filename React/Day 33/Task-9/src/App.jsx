d44a3a28e9bd9fa5b0f5ce473d4abbd28e0b1c9b
DEEPAN U <deepanudhayakumar01@gmail.com>
2026-01-26 22:22:38 +0530

    update

--- React/Day 33/Task-9/src/App.jsx
+++ React/Day 33/Task-9/src/App.jsx
@@ -2,17 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import InputPreview from './InputPreview'
-import "bootstrap/dist/css/bootstrap.min.css";
+import TextToggle from './TextToggle'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <InputPreview/>
-      
-    
+      <TextToggle/>
     </>
   )
 }
